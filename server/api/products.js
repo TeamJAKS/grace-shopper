9119c0a1696f5b0a52b0a941b5d2cf209836692d
Almendra <estrada.almendra@gmail.com>
2018-09-11 15:25:05 -0400

    Added Products Routes

---
+++ server/api/products.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+const Sequelize = require('sequelize')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+      res.status(404).send('Not Found')
+    } else {
+      res.json(product)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:category', async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err)
+  }
+})
