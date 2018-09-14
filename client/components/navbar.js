618970060722090d66e9e6a05d494db5f49cc7d2
Jordan Davis <jtdavis9311@gmail.com>
2018-09-14 16:24:54 -0400

    logout and create user is working

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -9,8 +9,8 @@ const defaultState = {
 }
 
 class Navbar extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = defaultState
     this.handleChange = this.handleChange.bind(this)
   }
@@ -26,11 +26,11 @@ class Navbar extends React.Component {
       <div>
         <h1>BOILERMAKER</h1>
         <nav>
-          {this.isLoggedIn ? (
+          {this.props.isLoggedIn ? (
             <div>
               {/* The navbar will show these links after you log in */}
               <Link to="/home">Home</Link>
-              <a href="#" onClick={this.handleClick}>
+              <a href="#" onClick={this.props.handleClick}>
                 Logout
               </a>
             </div>
